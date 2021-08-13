f200e2e9b9bf67015b1cfc7ea81a40370a70ad84
nargok <nargok@gmail.com>
2021-08-13 23:49:59 +0900

    Initial Commit

---
+++ github_search_app/github_search_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_search_appApp.swift
+//  github_search_app
+//
+//  Created by 後閑諒一 on 2021/08/13.
+//
+
+import SwiftUI
+
+@main
+struct github_search_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
