ef9bd47fcbf76910c9b8db65c0213ec8b1b328e9
nargok <nargok@gmail.com>
2021-08-21 12:04:17 +0900

    Add HomeView

---
+++ github_search_app/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  github_search_app
+//
+//  Created by 後閑諒一 on 2021/08/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN*//*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*//*@END_MENU_TOKEN*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
